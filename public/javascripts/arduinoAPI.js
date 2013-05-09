256135f8e19da6cc66060519b92b95c22da8e2e3
Lars Toft Jacobsen <lars@boxed.dk>
2013-05-09 14:22:49 +0200

    API comments and improvements

--- public/javascripts/arduinoAPI.js
+++ public/javascripts/arduinoAPI.js
@@ -29,55 +29,68 @@ socket.on('pin-update', function(data) {
 	pins[data.pin] = data.obj;
 });
 
-// query status
+// query status to fetch version and pin layout
 socket.emit('get-status', {});
 
 /*
  * Arduino/Firmata functions
  */
+
+ // digital write - toggle logic level (1 or 0) on digital pin
 function digitalWrite(pin, value) {
 	pins[pin].value = value;
 	socket.emit('digital-write', { pin: pin, value: value });
 }
 
+// analog write - set PWM value (8bit, 0-255) on PWM pin
 function analogWrite(pin, value) {
 	pins[pin].value = value;
 	socket.emit('analog-write', { pin: pin, value: value });
 }
 
+// set pin mode
 function pinMode(pin, mode) {
+	pins[pin].mode = mode;
 	socket.emit('digital-write', { pin: pin, mode: mode });
 }
 
+// set callback for specific analog pin read event
 function analogRead(pin, callback) {
 	socket.on('analog-read-'+pin, function(data) {
 		callback(data);
 	});
 }
 
+// set callback for all analog read events
 function analogReadAll(callback) {
 	socket.on('analog-read', function(data) {
 		callback(data);
 	});
 }
 
+// set callback for specific digital pin read event
 function digitalRead(pin, callback) {
 	socket.on('digital-read-'+pin, function(data) {
 		callback(data);
 	});
 }
 
+// set callback for all digital read events
 function digitalReadAll(callback) {
 	socket.on('digital-read', function(data) {
 		callback(data);
 	});
 }
 
+// set the firmata sampling interval
+// system default is 100ms, sketch is 19ms no less than 10ms
 function samplingInterval(interval) {
 	socket.emit('sampling-interval', interval);
 }
 
+// turn analog pin reporting on and off
 function reportAnalogPin(pin, report) {
+	pins[pin].report = report;
 	socket.emit('report-analog-pin', {pin: pin, value: report});
 }
 
